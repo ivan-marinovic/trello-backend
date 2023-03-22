b3ab5202b18d69bcd3a6dc8a8c87bf64f6a6f18b
ivan-marinovic <ivan.marinovic777@gmail.com>
2023-03-22 22:14:55 +0100

    init project

---
+++ src/main/java/com/example/Trellobackend/TrelloBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.Trellobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrelloBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrelloBackendApplication.class, args);
+	}
+
+}
