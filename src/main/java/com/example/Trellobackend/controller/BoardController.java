f3377cf3ce71adf78ef6209aca43186a784555b3
ivan-marinovic <ivan.marinovic777@gmail.com>
2023-03-27 00:40:56 +0200

    add CorsConfig class

--- src/main/java/com/example/Trellobackend/controller/BoardController.java
+++ src/main/java/com/example/Trellobackend/controller/BoardController.java
@@ -14,7 +14,6 @@ import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/board")
-@CrossOrigin(origins = "http://localhost:4200")
 public class BoardController {
 
     private final BoardService boardService;
